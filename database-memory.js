9df1895ebe6ca744db85f3582b1f43a92f81d8d1
Gustavo <118356161+torneira@users.noreply.github.com>
2023-12-07 10:46:53 -0400

    Aula 07/12/23

---
+++ database-memory.js
@@ -0,0 +1,38 @@
+import { randomUUID } from "crypto"
+
+
+export class DatabaseMemory{
+    #livros = new Map()
+
+list(){
+    return Array.from(this.#livros.entries()).map((livroArray) => {
+        const id =  livroArray[0]
+
+        const data = livroArray[1]
+
+        return{
+            id,
+            ...data,
+        }
+        .filter(livro => {
+            if (search){
+                return livro.titulo.includes(search)
+                
+            }
+        })
+    })
+}
+
+    create(livro){
+        const livroId = randomUUID()
+        this.#livros.set(livroId, livro)
+    }
+    
+    update(id, livro){
+        this.#livros.set(id, livro)
+    }
+
+    delete(id, livro){
+        this.#livros.delete(id, livro)
+    }
+}
